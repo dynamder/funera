f8083cb6399d3a3e5cfe289721b02552811b67e7
dynamder <igaster@163.com>
2026-05-24 17:13:44 +0800

    refactor(session): use ReActLoopHandle

--- src/chat/session.rs
+++ src/chat/session.rs
@@ -6,7 +6,10 @@ use serde::{Deserialize, Serialize};
 use tokio::{sync::mpsc, task::Id};
 use uuid::Uuid;
 
-use crate::chat::message::FuneraMessage;
+use crate::{
+    chat::message::FuneraMessage,
+    re_act::{ReActLoop, ReActLoopConfig, ReActLoopHandle},
+};
 use serde_json::{Value as JsonValue, json};
 
 //States
@@ -16,22 +19,25 @@ struct Running; //user prompted something that need call llms.
 impl State for Idle {}
 impl State for Running {}
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FuneraSession<SessionState: State> {
     id: Uuid,
     msgs: Arc<RwLock<Vec<FuneraMessage>>>,
     queued_msg: Arc<RwLock<Vec<FuneraMessage>>>,
+
+    #[serde(skip)]
+    current_loop: Option<ReActLoopHandle>,
+
     _state: PhantomData<fn() -> SessionState>,
 }
 
 impl<SessionState: State> FuneraSession<SessionState> {
-    pub fn session_context(&self) -> JsonValue {
+    pub fn session_context(&self) -> Vec<JsonValue> {
         self.msgs
             .read()
             .iter()
             .map(|msg| msg.format_json())
             .collect::<Vec<_>>()
-            .into()
     }
     pub fn id(&self) -> Uuid {
         self.id
@@ -44,6 +50,7 @@ impl FuneraSession<Idle> {
             id: Uuid::new_v4(),
             msgs: Arc::new(RwLock::new(Vec::new())),
             queued_msg: Arc::new(RwLock::new(Vec::new())),
+            current_loop: None,
             _state: PhantomData::<fn() -> Idle>,
         }
     }
@@ -53,13 +60,22 @@ impl FuneraSession<Idle> {
             id: self.id,
             msgs: self.msgs,
             queued_msg: self.queued_msg,
+            current_loop: None,
             _state: PhantomData::<fn() -> Running>,
         }
     }
 }
 
 impl FuneraSession<Running> {
-    pub fn react_loop(&self, init_msg: FuneraMessage) -> Result<()> {
+    pub async fn react_loop(
+        &mut self,
+        init_msg: FuneraMessage,
+        react_config: ReActLoopConfig,
+    ) -> Result<()> {
+        let react_loop = ReActLoop::from_config(react_config, self.session_context());
+        let queued_msg_sender = react_loop.sender();
+        let loop_handle = react_loop.run();
+        self.current_loop = Some(loop_handle);
         todo!()
     }
     pub fn idle(self) -> FuneraSession<Idle> {
@@ -67,7 +83,9 @@ impl FuneraSession<Running> {
             id: self.id,
             msgs: self.msgs,
             queued_msg: self.queued_msg,
+            current_loop: None,
             _state: PhantomData::<fn() -> Idle>,
         }
     }
 }
+//TODO: Session need an event loop to handle user commands(interactions)
--- src/re_act.rs
+++ src/re_act.rs
@@ -1,43 +1,82 @@
 use tokio::{sync::mpsc, task::JoinHandle};
+use tokio_util::sync::CancellationToken;
 
 use crate::chat::message::FuneraMessage;
 use anyhow::Result;
+use serde_json::Value as JsonValue;
 
 pub mod skills;
 pub mod tool;
 
+pub struct ReActLoopConfig {
+    pub buffer: usize,
+    pub max_iteration: usize,
+}
+impl ReActLoopConfig {
+    pub fn new(buffer: usize, max_iteration: usize) -> Self {
+        Self {
+            buffer,
+            max_iteration,
+        }
+    }
+}
+
 pub struct ReActLoop {
     buf_msg_rx: mpsc::Receiver<FuneraMessage>,
     buf_msg_tx: mpsc::Sender<FuneraMessage>,
+    history_msg: Vec<JsonValue>,
     max_iteration: usize,
 }
 impl ReActLoop {
-    pub fn new(buffer: usize, max_iteration: usize) -> Self {
+    pub fn new(buffer: usize, max_iteration: usize, history_msg: Vec<JsonValue>) -> Self {
         let (tx, rx) = mpsc::channel(buffer);
         Self {
             buf_msg_rx: rx,
             buf_msg_tx: tx,
+            history_msg,
             max_iteration,
         }
+    }
+    pub fn from_config(config: ReActLoopConfig, history_msg: Vec<JsonValue>) -> Self {
+        Self::new(config.buffer, config.max_iteration, history_msg)
     }
     pub fn sender(&self) -> mpsc::Sender<FuneraMessage> {
         self.buf_msg_tx.clone()
     }
-    pub fn run(mut self) -> JoinHandle<Result<()>> {
-        tokio::spawn(async move {
-            let self_tx = self.sender();
+    pub fn run(mut self) -> ReActLoopHandle {
+        let token = CancellationToken::new();
+        let token_clone = token.clone();
+
+        let sender = self.sender();
+        let self_tx = sender.clone();
+
+        let task = tokio::spawn(async move {
+            let loop_self_tx = self_tx;
             while let Some(msg) = self.buf_msg_rx.recv().await {
-                // TODO: 1. format msg and session context
+                // TODO: 1. format msg and session context, FuneraEnv
                 // TODO: 2. send formatted msg to llm api
                 // TODO: 3. receive response from llm api, execute tools(optional)
                 // TODO: 4. add tool reponse to buffered message channel
                 // TODO: 5. exit if channel is empty
                 // the message channel sender can be self_tx, or user queued message.
+
                 if self.buf_msg_rx.is_empty() {
                     break;
                 }
             }
             Ok(())
-        })
+        });
+        ReActLoopHandle {
+            cancel_token: token_clone,
+            task,
+            sender,
+        }
     }
+}
+
+#[derive(Debug)]
+pub struct ReActLoopHandle {
+    pub cancel_token: CancellationToken,
+    pub task: JoinHandle<Result<()>>,
+    pub sender: mpsc::Sender<FuneraMessage>,
 }
